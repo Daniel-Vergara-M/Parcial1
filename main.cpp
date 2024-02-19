9da3c2dddf723cac61eb58d4ce6a4e7d8229c340
Prestamo Estudiantes <prestamour@urosario.edu.co>
2024-02-19 15:00:07 -0500

    hilos

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
-
+#include <thread>
+#include <stdlib.h>
 using namespace std;
 
-int main() {
-    
+void threadOne(string *clave)
+{
+    srand(0);
+    this_thread::sleep_for(std::chrono::seconds(5));
+    *clave = "prueba";
+    cout << rand()%100 << endl;
+}
+
+int main()
+{
+    string clave = "trabajo";
+    thread first(threadOne, &clave);
+
+    first.join();
+    cout << clave << endl;
+    cout << "Final de los hilos";
+
     return 0;
 }
