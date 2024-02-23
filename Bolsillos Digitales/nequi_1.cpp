9f87192bca544d2a23137676f711f1aa90a354e4
Daniel Sanchez <ndanisanchez0525@gmail.com>
2024-02-23 01:22:51 -0500

    Implementando funciones en el main

--- Bolsillos Digitales/nequi_1.cpp
+++ Bolsillos Digitales/nequi_1.cpp
@@ -9,6 +9,7 @@ Dirigido a: @Tlcabrera
 #include <iostream>  // Se importa la librería para inputs y outputs
 #include <string>    // Se importa la librería para el manejo de strings
 #include <map>       // Se importa la librería map para hacer diccionarios
+#include <thread>    // Se importa la libreria para el manejo de hilos concurrentes
 #include "methods.h" // Se importa el archivo methods.h que contiene los métodos de impresión
 
 using namespace std;
@@ -51,6 +52,115 @@ string movimientosCuenta[20][6];
 2. "Transferencia",
 3. "Pago"
 */
+
+int dinamicPassword = (rand() % 900000 + 100000); // Variable global para almacenar la contraseña dinámica
+const int directions[5][4] = {
+    {1, 11, 23, 12},
+    {2, 11, 3, 57},
+    {3, 19, 1, 96},
+    {4, 19, 7, 29},
+    {5, 95, 11, 49}};
+/*Matriz que almacena las direcciones de los establecimientos
+ ID
+ CALLE
+ CARRERA
+ # ESTABLECIMIENTO*/
+
+// Función para generar la contraseña dinámica
+void generateDinamicPassword(int *password)
+{
+    while (true)
+    {
+        this_thread::sleep_for(chrono::seconds(3)); // Detiene el hilo de la generacion de la clave dinamica por 3 segundos
+        srand(time(nullptr));                       // Inicializa el generador de números aleatorios
+        *password = (rand() % 900000 + 100000);     // Genera un número aleatorio de 6 dígitos
+    }
+}
+
+// Función para inicializar el hilo de la contraseña dinámica
+void initThreadDinamicPassword()
+{
+    thread threadDinamicPassword(generateDinamicPassword, &dinamicPassword); // Inicia un hilo para generar la contraseña dinámica
+    threadDinamicPassword.detach();                                          // separa el hilo principal del hilo de la contraseña dinámica
+}
+
+// Función para calcular las indicaciones de dirección
+void calculateIndications(int *calleOrigen, int *carreraOrigen, int *numeroOrigen, int *selectedDirection)
+{
+    // Calcula la cantidad de cuadras en la calle y carrera
+    int blocksStreet = abs(*calleOrigen - *(*(directions + *selectedDirection) + 1));
+    int blocksCra = abs(*carreraOrigen - *(*(directions + *selectedDirection) + 2));
+
+    // Imprime las indicaciones de dirección
+    cout << "Desde la ubicación ";
+    cout << "Calle " << *calleOrigen << " Carrera " << *carreraOrigen << " #" << *numeroOrigen;
+    cout << " hasta la ubicación ";
+    cout << "Calle " << *(*(directions + *selectedDirection) + 1) << " Carrera " << *(*(directions + *selectedDirection) + 2) << " #" << *(*(directions + *selectedDirection) + 3) << ":\n";
+
+    // Imprime la primera indicación
+    cout << "1. Dirígase hacia "; 
+    if (*calleOrigen < *(*(directions + *selectedDirection) + 1))
+    {
+        cout << "el sur ";
+    }
+    else
+    {
+        cout << "el norte ";
+    }
+    cout << "por " << blocksStreet << " cuadras en la Calle " << *calleOrigen << ".\n";
+
+    // Imprime la segunda indicación
+    cout << "2. Gire a la ";
+    if (*carreraOrigen < *(*(directions + *selectedDirection) + 2))
+    {
+        cout << "derecha ";
+    }
+    else
+    {
+        cout << "izquierda ";
+    }
+
+    cout << "en la Carrera " << *carreraOrigen << " y avance ";
+    cout << blocksCra << " cuadras hacia ";
+    if (*carreraOrigen < *(*(directions + *selectedDirection) + 2))
+    {
+        cout << "el occidente.\n";
+    }
+    else
+    {
+        cout << "el oriente.\n";
+    }
+
+    // Imprime la tercera indicación
+    cout << "3. Busque el número de establecimiento " << *(*(directions + *selectedDirection) + 3) << " en la Calle " << *(*(directions + *selectedDirection) + 1) << ".\n";
+}
+
+// Función para buscar el cajero
+void searchCashier()
+{
+    int street, cra, number, selectedDirection;
+    // Solicita al usuario ingresar la calle, carrera y número de establecimiento
+    cout << "Ingrese la calle en la que se encuentra: " << endl;
+    cin >> street;
+
+    cout << "Ingrese la carrera en la que se encuentra: " << endl;
+    cin >> cra;
+
+    cout << "Ingrese el numero de establecimiento en el que se encuentra: " << endl;
+    cin >> number;
+
+    cout << "Seleccione alguna de las siguientes ubicaciones de cajeros: " << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << *(*(directions + i)) << ". ";
+        cout << "Calle: " << *(*(directions + i) + 1);
+        cout << " Carrera: " << *(*(directions + i) + 2);
+        cout << " Numero Establecimiento: " << *(*(directions + i) + 3) << endl;
+    }
+    cin >> selectedDirection;
+    selectedDirection = selectedDirection - 1;
+    calculateIndications(&street, &cra, &number, &selectedDirection); // Calcula las indicaciones para llegar al cajero
+}
 
 void getMovementHistory(string *phoneNumber)
 {
@@ -328,18 +438,20 @@ bool login(string *username, string *phoneNumber, string *pass)
 
 void onLoginMenu(string *phoneNumber)
 {
+    initThreadDinamicPassword();
     int option = 0;
     string amount, description, toPhoneNumber, newPass, username, pass, questionId, questionAnswer;
     do
     {
         println("=== MENU ===");
+        cout << "Su clave dinamica es: " << dinamicPassword << endl;
         println("1. Consultar saldo");
         println("2. Realizar transferencia");
         println("3. Retirar dinero");
         println("4. Depositar dinero");
         println("5. Pagar");
         println("6. Historial de movimientos");
-        println("7. Buscar puntos de retiro");
+        println("7. Buscar Cajeros Disponibles"); 
         println("8. Salir");
         print(" Seleccione una opción: ");
         cin >> option;
@@ -376,7 +488,7 @@ void onLoginMenu(string *phoneNumber)
             getMovementHistory(phoneNumber);
             break;
         case 7:
-            println("Saliendo del programa.");
+            searchCashier();
             break;
         case 8:
             println("Saliendo del programa.");
@@ -385,7 +497,7 @@ void onLoginMenu(string *phoneNumber)
             println("Opción no válida");
             break;
         }
-    } while (option != 7);
+    } while (option != 8);
 }
 void loginMenu()
 {
@@ -422,10 +534,23 @@ void registerMenu()
     string usuario, numCelular, clave;
     print("Ingrese su usuario: ");
     cin >> usuario;
+
     print("Ingrese su número de celular: ");
     cin >> numCelular;
+    if (numCelular.length() != 10)
+    {
+        println("El número de celular debe tener 10 digitos.");
+        return;
+    }
+
     print("Ingrese su clave: ");
     cin >> clave;
+    if (clave.length() != 4)
+    {
+        println("La clave debe tener 4 caracteres.");
+        return;
+    }
+
     registerAccount(&usuario, &numCelular, &clave);
 }
 
@@ -462,7 +587,7 @@ void mainMenu()
             break;
         case 2:
             registerMenu();
-            break;
+            break; 
         case 3:
             recoverPasswordMenu();
             break;
